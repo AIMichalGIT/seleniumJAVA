d7a6aeecefdc736b1f0ad75a3fedc53c472a3273
AIMichalGIT <michal.bylica1@gmail.com>
2024-08-05 21:29:47 +0200

    dodanie kilku klas

---
+++ src/test/java/Metody.java
@@ -0,0 +1,19 @@
+public class Metody {
+//    public void policzWynik() {
+//        System.out.println("Zaraz policzę wynik");
+//        int result = 0;
+//        for (int i=0; i<100; i++) {
+//            result = result+i;
+//        }
+//        System.out.println("Rezultat to: " + result);
+//    }
+public void policzWynikParam(int number) {
+    System.out.println("Numer ma wartość: " + number);
+    System.out.println("Zaraz policzę wynik");
+    int result = 0;
+    for (int i=0; i < number; i++) {
+        result = result + i;
+    }
+    System.out.println("Rezultat to: " + result);
+}
+}
---
+++ src/test/java/MetodyTest.java
@@ -0,0 +1,8 @@
+public class MetodyTest {
+    public static void main(String[] args) {
+        Metody metody = new Metody();
+//        metody.policzWynik();
+        metody.policzWynikParam(10);
+        metody.policzWynikParam(20);
+    }
+}
