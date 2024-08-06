0754756f1926bf659f6507059c6f957ce66720c2
AIMichalGIT <michal.bylica1@gmail.com>
2024-08-06 21:27:07 +0200

    getter i setter

---
+++ src/test/java/ReadOnly.java
@@ -0,0 +1,12 @@
+public enum ReadOnly {;
+    private String name = "Do odczytu tylko";
+    // getter opisany poniżej
+//    public String getName() {
+//        name = name + "jest ok";
+//        return name;
+//}
+    // setter poniżej
+    public void setName (String name) {
+    this.name = name;
+    }
+}
