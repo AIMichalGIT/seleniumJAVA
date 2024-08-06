c5eeac797f6b81860ebdb610f13d23e51df3317c
AIMichalGIT <michal.bylica1@gmail.com>
2024-08-06 13:51:52 +0200

    przeciazanie metod

---
+++ src/test/java/MethodNameTest.java
@@ -0,0 +1,8 @@
+public class MethodNameTest {
+    public static void main(String[] args) {
+        MethodName methodName = new MethodName();
+        methodName.add(1);
+        methodName.add(1,2);
+        methodName.add(1,2,3);
+    }
+}
