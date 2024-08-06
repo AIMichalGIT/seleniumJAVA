a92211af3372cba2b5dce0b04eff1ecf8d5dd218
AIMichalGIT <michal.bylica1@gmail.com>
2024-08-06 13:43:07 +0200

    zadanie app checker

---
+++ src/test/java/AppChecker.java
@@ -0,0 +1,11 @@
+public class AppChecker {
+
+    public static void main(String[] args) {
+        AndroidApp android = new AndroidApp("Calculator");
+        android.runAndroidApp();
+
+        IphoneApp iphoneApp = new IphoneApp("Calculator2");
+        iphoneApp.appInfo();
+        iphoneApp.runIphoneApp();
+    }
+}
