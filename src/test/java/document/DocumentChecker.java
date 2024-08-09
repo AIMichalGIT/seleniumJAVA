e2c7c6432e4f768ea6978f268d1d5a008ddd67de
AIMichalGIT <michal.bylica1@gmail.com>
2024-08-09 17:42:28 +0200

    wyjatki

---
+++ src/test/java/document/DocumentChecker.java
@@ -0,0 +1,9 @@
+package document;
+
+public class DocumentChecker {
+    public static void main(String[] args) {
+        ExcelDocument document = new ExcelDocument();
+        Document excelDocument = new ExcelDocument();
+        Document pdfDocument = new PdfDocument();
+    }
+}
